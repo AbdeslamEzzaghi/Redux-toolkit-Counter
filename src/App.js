8e58e988d6d52b7fa66bd5df504d99e4998e44cc
AbdeslamEzzaghi <60900845+AbdeslamEzzaghi@users.noreply.github.com>
2023-04-12 18:55:16 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Counter from "./components/Counter";
+import Auth from "./components/Auth";
+import Header from "./components/Header";
+function App() {
+  return (
+    <>
+      <Counter />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Counter.js
@@ -0,0 +1,37 @@
+import classes from "./Counter.module.css";
+import { useSelector, useDispatch } from "react-redux";
+
+import { counterActions } from "../store/index";
+
+const Counter = () => {
+  const counter = useSelector((state) => state.counter);
+  const show = useSelector((state) => state.showCounter);
+  const dispatch = useDispatch();
+
+  const incerementHandler = (val) => {
+    console.log(val);
+    dispatch(counterActions.increment(val));
+  };
+  const decerementHandler = (val) => {
+    dispatch(counterActions.decrement());
+  };
+  const toggleCounterHandler = () => {
+    dispatch(counterActions.toggle());
+  };
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      {show && <div className={classes.value}>-- {counter} --</div>}
+      <div>
+        <button onClick={decerementHandler}>Decrement</button>
+        <button onClick={incerementHandler.bind(null, 5)}>
+          IncerementByFive
+        </button>
+        <button onClick={incerementHandler.bind(null, 1)}>Incerement</button>
+      </div>
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>
+  );
+};
+
+export default Counter;
