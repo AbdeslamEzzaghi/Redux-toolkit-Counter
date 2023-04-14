97e892f2abb8517bc77e55789c7311ade02ae660
AbdeslamEzzaghi <60900845+AbdeslamEzzaghi@users.noreply.github.com>
2023-04-14 15:00:09 +0000

    added new components to app.js and separated index.js content

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { useSelector } from "react-redux";
+
 import Counter from "./components/Counter";
 import Auth from "./components/Auth";
+
+import UserProfile from "./components/UserProfile";
 import Header from "./components/Header";
 function App() {
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+
   return (
     <>
+      <Header />
+      {isAuthenticated ? <UserProfile /> : <Auth />}
       <Counter />
     </>
   );
